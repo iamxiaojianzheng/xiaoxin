fb5cad0568a3098a066ddbdd184f163b3bd0a787
JIAHE <1272209235@qq.com>
2021-11-20 16:45:45 +0800

    优化录屏调用方法

--- src/main/java/cn/xiaojianzheng/xiaoxin/video/WindowsScreenRecording.java
+++ src/main/java/cn/xiaojianzheng/xiaoxin/video/WindowsScreenRecording.java
@@ -1,19 +1,30 @@
 package cn.xiaojianzheng.xiaoxin.video;
 
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.thread.ThreadUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.bytedeco.ffmpeg.global.avcodec;
 import org.bytedeco.ffmpeg.global.avutil;
 import org.bytedeco.javacv.*;
+
+import java.nio.file.Path;
+import java.util.Objects;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author JIAHE
  * @since 1.0
  */
+@Slf4j
 public class WindowsScreenRecording {
     private static FFmpegFrameGrabber grabber;
     private static FFmpegFrameRecorder recorder;
 
+    private static final ExecutorService executorService = ThreadUtil.newExecutor(1);
     private static final int frameRate = 24;
-    private static volatile boolean isRunning = false;
+
+    public static volatile boolean isRunning = false;
 
     static {
         try {
@@ -21,47 +32,56 @@ public class WindowsScreenRecording {
             FFmpegFrameRecorder.tryLoad();
         } catch (Exception ignored) {
         }
+
+        Runtime.getRuntime().addShutdownHook(new Thread(WindowsScreenRecording::close, "closeScreenRecording"));
     }
 
-    private void initGrabber() throws FFmpegFrameGrabber.Exception {
-        grabber = new FFmpegFrameGrabber("desktop");
-        grabber.setFormat("gdigrab");
-        grabber.setOption("fflags", "nobuffer");
-        grabber.start();
+    private static void initGrabber() throws FrameGrabber.Exception {
+        if (grabber == null) {
+            grabber = new FFmpegFrameGrabber("desktop");
+            grabber.setFormat("gdigrab");
+            grabber.setOption("fflags", "nobuffer");
+            grabber.start();
+        } else {
+            grabber.restart();
+        }
     }
 
-    private void initRecorder(String savePath) throws FFmpegFrameRecorder.Exception {
-        // 流媒体输出地址，分辨率（长，高），是否录制音频（0:不录制/1:录制）
-        recorder = new FFmpegFrameRecorder(savePath, grabber.getImageWidth(), grabber.getImageHeight(), grabber.getAudioChannels());
+    private static void initRecorder(String savePath) throws FrameRecorder.Exception {
+        if (recorder == null) {
+            recorder = new FFmpegFrameRecorder(savePath, grabber.getImageWidth(), grabber.getImageHeight(), grabber.getAudioChannels());
+        }
+
         // 是否支持flv
         if (supportFlvFormatCodec()) {
             // 设置封装格式
             recorder.setFormat("flv");
-            recorder.setFrameRate(frameRate);
-            recorder.setGopSize(frameRate); // 设置gop,与帧率相同，相当于间隔1秒产生一个关键帧
-            recorder.setVideoOption("crf", "26");
-            recorder.setVideoOption("threads", "1");
-            recorder.setVideoOption("tune", "zerolatency");
-            recorder.setVideoOption("preset", "ultrafast");
-            recorder.setMetadata(grabber.getMetadata());
-            // 设置视频格式
-            recorder.setVideoCodec(avcodec.AV_CODEC_ID_H264);
-            recorder.setPixelFormat(avutil.AV_PIX_FMT_YUV420P);
-            recorder.setAudioCodec(avcodec.AV_CODEC_ID_AAC);
-            // 启用RDOQ算法，优化视频质量
-            // 1：在视频码率和视频质量之间取得平衡
-            // 2：最大程度优化视频质量（会降低编码速度和提高码率）
-            recorder.setTrellis(1);
-            recorder.setMaxDelay(0);
         }
+
+        recorder.setFrameRate(frameRate);
+        recorder.setGopSize(frameRate); // 设置gop,与帧率相同，相当于间隔1秒产生一个关键帧
+        recorder.setVideoOption("crf", "26");
+        recorder.setVideoOption("threads", "1");
+        recorder.setVideoOption("tune", "zerolatency");
+        recorder.setVideoOption("preset", "ultrafast");
+        recorder.setMetadata(grabber.getMetadata());
+        // 设置视频格式
+        recorder.setVideoCodec(avcodec.AV_CODEC_ID_H264);
+        recorder.setPixelFormat(avutil.AV_PIX_FMT_YUV420P);
+        recorder.setAudioCodec(avcodec.AV_CODEC_ID_AAC);
+        // 启用RDOQ算法，优化视频质量
+        // 1：在视频码率和视频质量之间取得平衡
+        // 2：最大程度优化视频质量（会降低编码速度和提高码率）
+        recorder.setTrellis(1);
+        recorder.setMaxDelay(0);
+
         // 开始录制
         recorder.start();
     }
 
-    private void recordFrame() {
-//        int poll = 1000 / frameRate;
-//        ScheduledThreadPoolExecutor executorService = ThreadUtil.createScheduledExecutor(1);
-//        executorService.scheduleAtFixedRate(() -> {
+    private static void recordFrame() {
+        isRunning = true;
+
         Frame frame;
 
         // 时间戳计算
@@ -82,43 +102,58 @@ public class WindowsScreenRecording {
             }
         } catch (FrameGrabber.Exception | FrameRecorder.Exception ignored) {
         }
-//        }, poll, poll, TimeUnit.MILLISECONDS);
     }
 
-    public void start(String savePath) {
-        try {
-            initGrabber();
-            initRecorder(savePath);
-            isRunning = true;
-            recordFrame();
-        } catch (FFmpegFrameGrabber.Exception | FFmpegFrameRecorder.Exception e) {
-            e.printStackTrace();
-        } finally {
+    public static void start(Path savePath) {
+        if (!isRunning) {
+            FileUtil.mkdir(savePath.getParent());
+            String canonicalPath = FileUtil.getCanonicalPath(savePath.toFile());
+
             try {
-                if (grabber != null) grabber.close();
-                if (recorder != null) recorder.close();
-            } catch (Exception ignored) {
+                initGrabber();
+                initRecorder(canonicalPath);
+                executorService.execute(WindowsScreenRecording::recordFrame);
+                log.info("开始录屏：{}", canonicalPath);
+            } catch (FrameGrabber.Exception | FrameRecorder.Exception e) {
+                log.error(e.getMessage());
+                close();
             }
+
         }
     }
 
-    public void stop() {
+    public static void stop() {
+        synchronized (log) {
+            isRunning = false;
+            log.info("close screen recording: {}", isRunning);
+            while (executorService.isShutdown()) {
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException ignored) {
+                }
+            }
+            close();
+        }
+    }
+
+    private static void close() {
         try {
-            recorder.close();
-            grabber.close();
+            if (recorder != null) {
+                recorder.close();
+                recorder = null;
+            }
+            if (grabber != null) {
+                grabber.close();
+            }
         } catch (Exception e) {
-            e.printStackTrace();
+            log.error(e.getMessage(), e);
         }
-    }
-
-    public static boolean isRunning() {
-        return isRunning;
     }
 
     /**
      * 是否支持flv的音视频编码
      */
-    private boolean supportFlvFormatCodec() {
+    private static boolean supportFlvFormatCodec() {
         int vcodec = grabber.getVideoCodec();
         int acodec = grabber.getAudioCodec();
         return (avcodec.AV_CODEC_ID_H264 == vcodec || avcodec.AV_CODEC_ID_H263 == vcodec)
--- src/test/java/cn/xiaojianzheng/xiaoxin/video/WindowsScreenRecordingTest.java
+++ src/test/java/cn/xiaojianzheng/xiaoxin/video/WindowsScreenRecordingTest.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
 /**
@@ -18,7 +19,7 @@ class WindowsScreenRecordingTest {
         String savePath = new File("video/test.mp4").getAbsolutePath();
         WindowsScreenRecording recording = new WindowsScreenRecording();
 
-        ThreadUtil.execute(() -> recording.start(savePath));
+        ThreadUtil.execute(() -> recording.start(Paths.get(savePath)));
 
         log.info("****stop停止录制****");
         while (true) {
